93ff071f2ab3a5c8babd0bb71e07a197f4b5f19f
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-09 15:07:32 -0600

    - Added incomplete base functionality of Galactic Crusade Spawner.

--- Assets/Devs/Dev_Lorenzo/GalacticCrusadeSpawner.cs
+++ Assets/Devs/Dev_Lorenzo/GalacticCrusadeSpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GalacticCrusadeSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _npcToSpawn;
+    [SerializeField] private float _spawnInterval = 1f;
+    [SerializeField] private float _spawnDelay = 0f;
+    [SerializeField] private int _maxSpawnCount = 1;
+
+    private Vector2 _spawnArea;
+    private float _spawnTimer = 0f;
+
+    public void InitializeSpawner(Vector2 newSpawnArea)
     {
-        
+        _spawnArea = newSpawnArea;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_spawnTimer >= _spawnInterval)
+        {
+            // Spawn units here.
+            _spawnTimer = 0f;
+        }
+        else _spawnTimer += Time.deltaTime;
+    }
+
+    public void StartSpawner()
+    {
+        this.enabled = true;
+        _spawnTimer = 0f;
+    }
+
+    public void EndSpawner()
+    {
+        this.enabled = false;
     }
 }
