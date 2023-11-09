ec28f8acdc7d7f3e2dc0ccbfad947844d6f315d7
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-09 16:03:07 -0600

    - Tested GalacticCrusade ability. - Added placeholder functionality to GalacticCrusadeShip.

--- Assets/Devs/Dev_Lorenzo/GalacticCrusadeShip.cs
+++ Assets/Devs/Dev_Lorenzo/GalacticCrusadeShip.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GalacticCrusadeShip : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.Translate(Vector2.up * Time.deltaTime * 5f);
     }
 }
