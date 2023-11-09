e79a92dee0a73caa1e7d79b6cfdd2ffb41c2204e
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-11-09 14:39:38 -0600

    bomb now shoots forward for the duration of windup, and then blows up on action.

--- Assets/Devs/Dev_Craig/Ability_Bomb.cs
+++ Assets/Devs/Dev_Craig/Ability_Bomb.cs
@@ -7,6 +7,9 @@ public class Ability_Bomb : Ability_Simple
     private CircleCollider2D bombCollider;
     private SpriteRenderer spriteRenderer;
     private CircleCollider2D spawnedCollider;
+    private Rigidbody2D bombRigidBody;
+    private float bombLaunchSpeed = 1500f;
+    private bool doOnce = true;
 
     //Acquire virtual functions.
     private void Awake()
@@ -20,7 +23,14 @@ public class Ability_Bomb : Ability_Simple
         base.StartWindup();
         //Instansiate Projectile
         GameObject spawnedBomb = Instantiate(this.gameObject, transform.parent.GetComponent<Transform>().position, Quaternion.identity);
-        Debug.Log(transform.parent.GetComponent<Transform>().position);
+        bombRigidBody = spawnedBomb.GetComponent<Rigidbody2D>();
+
+        if (doOnce)
+        {
+            doOnce = false;
+            bombRigidBody.AddForce(Vector2.up * bombLaunchSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
         spriteRenderer = spawnedBomb.GetComponent<SpriteRenderer>();
         spawnedCollider = spawnedBomb.GetComponent <CircleCollider2D>();
         spriteRenderer.enabled = true;
@@ -28,14 +38,19 @@ public class Ability_Bomb : Ability_Simple
     protected override void StartFiring()
     {
         base.StartFiring();
+        doOnce = true;
         //Enable collider
         if(spawnedCollider != null)
-        {
             spawnedCollider.enabled = true;
-        }
-        
 
+        //Stop projectile from moving
+        if(bombRigidBody != null)
+            bombRigidBody.velocity = Vector2.zero;
 
+        //Turn off the bomb sprite
+        if(spriteRenderer != null)
+            spriteRenderer.enabled = false;
+        //Turn on the Explosion animation
     }
 
     protected override void StartWinddown()
