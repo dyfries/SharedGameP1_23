9e87211de6d1099c60b1034db5134b209e7bfd43
Quinton78 <qpalmisani@rrc.ca>
2023-11-14 14:36:53 -0600

    Began animation states for my shield

--- Assets/Devs/Dev_Quentin/Ability_Block.cs
+++ Assets/Devs/Dev_Quentin/Ability_Block.cs
@@ -8,16 +8,33 @@ public class Ability_Block : Ability_Simple
     public GameObject Block;
     public GameObject BrokenBlock;
 
+    [Header(" --- Animator ---  ")]
+    public Animator anim;
+    private string anim_windupString = "Lazer_Windup";
+    private string anim_firingString = "Lazer_Firing";
+    private string anim_winddownString = "Lazer_Winddown";
+
     // For refrence, the way the shield should work is that the shield animtion will play when you press a button.
     // Then when it hits "StartWinddown" it will play the recharge animation for a set amount of time.
     // After that set amount of time has passed there would be a visual effect that would play, showing that you can use the shield again.
 
+    protected void Start()
+    {
+        if (anim == null)
+        {
+            anim = GetComponentInChildren<Animator>();
+            if (anim == null)
+            {
+                Debug.LogWarning("Animator cannot be found by Huge Lazer ability");
+            }
+        }
+    }
     protected override void StartWindup()
     {
         base.StartWindup();
 
 
-        //anim.SetBool(anim_windupString, true);
+        anim.SetBool(anim_windupString, true);
 
     }
 
@@ -27,7 +44,9 @@ public class Ability_Block : Ability_Simple
         Block.GetComponent<CircleCollider2D>().enabled = true;
         BrokenBlock.SetActive(false);
         // When in StartFiring shield is fully charged
-        
+        anim.SetBool(anim_windupString, false);
+        anim.SetBool(anim_firingString, true);
+
     }
 
     protected override void StartWinddown()
@@ -37,13 +56,15 @@ public class Ability_Block : Ability_Simple
         BrokenBlock.SetActive(true);
         Block.SetActive(false);
         // When StartWinddown is active then shield will entire it's broken/recharge state
+        anim.SetBool(anim_firingString, false);
+        anim.SetBool(anim_winddownString, true);
     }
 
     protected override void StartCooldown()
     {
         base.StartCooldown();
 
-        //anim.SetBool(anim_winddownString, false);
+        anim.SetBool(anim_winddownString, false);
 
     }
 }
