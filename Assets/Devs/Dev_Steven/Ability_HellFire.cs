2fd0636a0cb6420c97a8e77d92c0242d05c54ba1
Steven Laird <144483744+StevenLairdRRC@users.noreply.github.com>
2023-11-15 14:48:21 -0600

    Crosshair fixed, now a prefab. Projectile rotations implemented.

--- Assets/Devs/Dev_Steven/Ability_HellFire.cs
+++ Assets/Devs/Dev_Steven/Ability_HellFire.cs
@@ -8,18 +8,16 @@ public class Ability_HellFire : Ability_Simple
     public GameObject projectilePrefab;
     public GameObject explosionPrefab;
 
-    //public float growSpeed = 4f;
-    //public float maxSize = 1f;
-    //public float destroyAbilityIn = 2f;
-
-    public int projectileCount;
-    public float projectileSpeed;
+    public int projectileCount = 10;
+    public float projectileSpeed = 2f;
+    public float rotationSpeed = 2f;
     private float angleStep;
 
     public List<GameObject> projectiles;
 
-    public GameObject targetCrosshair;
-    
+    private Vector3 targetPosition;
+    public GameObject crosshairPrefab;
+
 
     private void Awake()
     {
@@ -28,7 +26,6 @@ public class Ability_HellFire : Ability_Simple
         {
             Debug.LogError(gameObject.name + " can't locate SpriteRenderer in children of parent object.");
         }
-        targetCrosshair.SetActive(false);
     }
 
     protected void Start()
@@ -38,14 +35,66 @@ public class Ability_HellFire : Ability_Simple
 
     protected override void Update()
     {
+        if (stageOfAbility == StageOfAbility.firing)
+        {
+            for (int i = 0; i < projectiles.Count; i++)
+            {
+                if (projectiles[i] != null)
+                {
+                    Vector3 rotateTowards = (targetPosition - projectiles[i].transform.position).normalized;
+                    float smoothRotateAngle = Vector2.SignedAngle(projectiles[i].transform.up, rotateTowards);
+
+                    if (smoothRotateAngle > 0)
+                    {
+                        // Rotate Left
+                        projectiles[i].transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+                    }
+                    else if (smoothRotateAngle < 0)
+                    {
+                        // Rotate Right
+                        projectiles[i].transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+                    }
+                }
+            }
+        }
+
+        if (stageOfAbility == StageOfAbility.windup 
+            || stageOfAbility == StageOfAbility.firing 
+            || stageOfAbility == StageOfAbility.winddown)
+        {
+            for (int i = 0; i < projectiles.Count; i++)
+            {               
+                if (projectiles[i] != null)
+                {
+                    //projectiles[i].GetComponent<Rigidbody2D>().AddForce(projectiles[i].transform.up * projectileSpeed, ForceMode2D.Impulse);
+                    projectiles[i].transform.position = projectiles[i].transform.position + projectiles[i].transform.up * projectileSpeed * Time.deltaTime;                    
+                }
+            }
+        }
+
+        for (int i = 0; i < projectiles.Count; i++)
+        {
+            if (projectiles[i] != null)
+            {
+                float distanceFromTarget = Vector2.Distance(targetPosition, projectiles[i].transform.position);
+
+                if (distanceFromTarget < 0.3f)
+                {
+                    Destroy(projectiles[i]);
+
+                    GameObject explosion;
+                    explosion = Instantiate(explosionPrefab, projectiles[i].gameObject.transform.position, projectiles[i].gameObject.transform.rotation);
+                    Destroy(explosion, 1.5f);
+                }
+            }
+        }
+
         base.Update();
     }
 
     protected override void StartReady()
     {
-        playerSpriteRenderer.color = Color.white;
-
-        angleStep = 360f / projectileCount;
+        playerSpriteRenderer.color = Color.white;        
 
         base.StartReady();
     }
@@ -54,16 +103,22 @@ public class Ability_HellFire : Ability_Simple
     {
         playerSpriteRenderer.color = Color.green;
 
-        targetCrosshair.SetActive(true);
+        targetPosition = transform.position + Vector3.up * 3f;
+
+        GameObject crosshair;
+        crosshair = Instantiate(crosshairPrefab, targetPosition, Quaternion.identity);
+        Destroy(crosshair, 3f);
 
         for (int i = 0; i < projectileCount; i++)
         {
+            angleStep = 360f / projectileCount;
             float angle = i * angleStep;
             Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
 
             GameObject projectile;
             //projectile = Instantiate(projectilePrefab, transform.position, Random.rotation, gameObject.transform);
-            projectile = Instantiate(projectilePrefab, transform.position, rotation, gameObject.transform);            
+            projectile = Instantiate(projectilePrefab, transform.position, rotation, gameObject.transform);
+            projectile.name = "Projectile " + i;
             projectiles.Add(projectile);
         }        
 
@@ -74,11 +129,10 @@ public class Ability_HellFire : Ability_Simple
     {
         playerSpriteRenderer.color = Color.red;
 
-        targetCrosshair.SetActive(false);
 
         for (int i = 0; i < projectiles.Count; i++)
         {
-            projectiles[i].GetComponent<Rigidbody2D>().AddForce(projectiles[i].transform.up * projectileSpeed, ForceMode2D.Impulse);
+            //projectiles[i].GetComponent<Rigidbody2D>().AddForce(projectiles[i].transform.up * projectileSpeed, ForceMode2D.Impulse);
         }
 
         base.StartFiring();
