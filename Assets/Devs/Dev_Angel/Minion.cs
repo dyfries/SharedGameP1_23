0f5706bd43593957ec92e1652dc10cf9e6497007
Angel Gamis <agamis@rrc.ca>
2023-11-16 14:23:16 -0600

    Adding self destruct feature and changed rigidbody variable name that was causing issues

--- Assets/Devs/Dev_Angel/Minion.cs
+++ Assets/Devs/Dev_Angel/Minion.cs
@@ -5,32 +5,45 @@ using UnityEngine;
 
 public class Minion : MonoBehaviour
 { 
+	[Header("Move")]
+	private Rigidbody2D rigid;
 	private Vector3 currentPosition;
 	private Vector3 closestEnemyPosition;
 	private Vector3 moveTowards;
-	[SerializeField] private float findEnemyRadius;
-	private Rigidbody2D rigidbody;
-
-	[SerializeField]private BaseNPC npc;
-
-	[SerializeField]private float moveForce = 10;
+	[SerializeField] private float moveForce = 10;
 	private float movingTimer;
+
+	[Header("Find NPC")]
+	private BaseNPC npc;
+	private float findEnemyRadius = 15;
+
+	[Header("Self Destruct")]
+	private float destructTimer;
+	private float destructTimeAmount = 10;
 
 	private void Start()
 	{
-		rigidbody = GetComponent<Rigidbody2D>();
+		rigid = GetComponent<Rigidbody2D>();
 	}
 
 	private void Update()
 	{
 		movingTimer += Time.deltaTime;
+		destructTimer += Time.deltaTime;
 		currentPosition = transform.position;
 
+
+		// Find the closest enemy within range
 		FindClosestEnemy();
+
+		// Move towards the closest NPC enemy
 		MoveToClosestNPC();
 
+		// Self destruct after time
+		SelfDestruct();
     }
 
+	// If npc is null find a new closest enemy within range
 	private void FindClosestEnemy()
 	{
 		RaycastHit2D hit = Physics2D.CircleCast(currentPosition, findEnemyRadius, Vector2.zero);
@@ -51,9 +64,23 @@ public class Minion : MonoBehaviour
 		moveTowards = closestEnemyPosition - currentPosition;
 
 		// Add a move force in that direction
-		rigidbody.AddForce(moveTowards.normalized * moveForce * Time.deltaTime);
+		rigid.AddForce(moveTowards.normalized * moveForce);
 
 		Debug.Log(moveTowards.normalized);
         Debug.DrawRay(currentPosition, closestEnemyPosition);
+    }
+
+	private void HitNPC()
+    {
+
+    }
+
+	// After destructTimeAmount seconds self destruct
+	private void SelfDestruct()
+    {
+		if(destructTimer >= destructTimeAmount)
+        {
+			Destroy(gameObject);
+        }
     }
 }
