b398e4bc081ac4585a22caa6d63692fd9d2aaaef
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-11-16 13:33:20 -0600

    - Fixed crusade ship strafe movement.

--- Assets/Devs/Dev_Lorenzo/GalacticCrusadeShip.cs
+++ Assets/Devs/Dev_Lorenzo/GalacticCrusadeShip.cs
@@ -13,16 +13,32 @@ public class GalacticCrusadeShip : MonoBehaviour
     [SerializeField] private float _initialMovementDuration = 0.5f;
     [SerializeField] private float _firingDuration = 3f;
     [SerializeField] private float _exitLifetime = 10f;
+    [Header("Strafe Movement Settings")]
     [SerializeField] private float _sineMoveStrength = 180f;
     [Header("Weapon Settings")]
     [SerializeField, Min(0.01f)] private float _attackSpeed = 0.5f;        // In real seconds.
+    [Header("Debug mode helps visualize strafe movement.")]
+    [SerializeField] private bool _debugMode = false;
 
     private float _timer = 0f;
     private float _projectileTimer = 0f;
 
     private void Start()
     {
-        StartCoroutine(StartAttack());
+        if (!_debugMode)
+        {
+            StartCoroutine(StartAttack());
+        }
+    }
+
+    private void Update()
+    {
+        if (_debugMode)
+        {
+            float sinValue = Mathf.Sin(_timer);
+            transform.Translate(Vector2.right * _moveSpeed * (sinValue * Time.deltaTime));
+            _timer += Time.deltaTime * _sineMoveStrength;
+        }
     }
 
     private void Move()
@@ -44,13 +60,16 @@ public class GalacticCrusadeShip : MonoBehaviour
             _timer += Time.deltaTime;
             yield return null;
         }
+
         // Strafe left and right while firing projectiles.
+        float strafeTimer = 0f;
         _timer = 0f;
         while (_timer < _firingDuration)
         {
-            // Strafe.
-            float sinValue = Mathf.Sin(_timer * _sineMoveStrength);
-            transform.Translate(Vector2.right * sinValue * Time.deltaTime);
+            // Strafe movement using sine.
+            strafeTimer += Time.deltaTime * _sineMoveStrength;
+            float sinValue = Mathf.Sin(strafeTimer);
+            transform.Translate(Vector2.right * sinValue * _moveSpeed * Time.deltaTime);
 
             // Shoot projectiles.
             if (_projectileTimer >= _attackSpeed)
@@ -63,6 +82,7 @@ public class GalacticCrusadeShip : MonoBehaviour
             _timer += Time.deltaTime;
             yield return null;
         }
+
         // Continue moving until despawning.
         _timer = 0f;
         while (_timer < _exitLifetime)
