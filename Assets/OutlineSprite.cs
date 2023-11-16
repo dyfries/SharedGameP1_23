8ce165ee78305bd44ab16b8e8a32dbf159d3ba80
HeliturtleOP <oproulx@academic.rrc.ca>
2023-11-16 17:25:11 -0600

    Made outline generate procedurally

--- Assets/OutlineSprite.cs
+++ Assets/OutlineSprite.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OutlineSprite : MonoBehaviour
@@ -7,11 +5,26 @@ public class OutlineSprite : MonoBehaviour
 
     private SpriteRenderer sp;
     private SpriteMask[] spms;
+    private float pixelUnit;
+    private int[,] offsetMatrix = new int[2, 8] { { 0, 0, 1, 1, 1, -1, -1, -1 },
+                                                  { 1, -1, 0, 1, -1, 0, 1, -1 } };
 
     // Start is called before the first frame update
     void Start()
     {
+
         sp = transform.root.GetComponentInChildren<SpriteRenderer>();
+
+        pixelUnit = 1f / sp.sprite.pixelsPerUnit;
+
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject outline = Instantiate(new GameObject(i.ToString(), typeof(SpriteMask)),transform);
+            //outline.transform.parent = transform;
+
+            outline.transform.position = new Vector2(offsetMatrix[0, i] * pixelUnit, offsetMatrix[1, i] * pixelUnit);
+
+        }
 
         spms = GetComponentsInChildren<SpriteMask>();
 
