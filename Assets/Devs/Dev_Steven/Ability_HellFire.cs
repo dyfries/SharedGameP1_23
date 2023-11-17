adffcc31ad0aa1b14c8952ae1a979a0b9391e474
StevenLairdRRC <SLaird2@academic.rrc.ca>
2023-11-16 21:57:12 -0600

    Prefabs renamed. Method and Variable refactoring and organization, new Toggles. RayCast and OverlapCircle lock ons implemented.

--- Assets/Devs/Dev_Steven/Ability_HellFire.cs
+++ Assets/Devs/Dev_Steven/Ability_HellFire.cs
@@ -3,28 +3,46 @@ using UnityEngine;
 
 public class Ability_HellFire : Ability_Simple
 {
+    [Header("References & Layers")]
     private SpriteRenderer playerSpriteRenderer;
-
-    [Header("Prefab References")]
     public GameObject projectilePrefab;
     public GameObject explosionPrefab;
     public GameObject crosshairPrefab;
+    public LayerMask NPCLayers;
 
     [Header("Ability Settings")]
     [Range(1, 100)]
     public int projectileCount = 9;
-    [Range(0.1f, 5f)]
+    [Range(0.1f, 10f)]
     public float launchSpeed = 2f;
-    [Range(0.1f, 5f)]
+    [Range(0.1f, 10f)]
     public float targetSpeed = 4f;
-    [Range(2f, 10f)]
+    [Range(1f, 8f)]
     public float targetDistance = 4f;
-    private Vector3 targetPosition;
+    private Vector3 crosshairPosition;
     [Range(0f, 300f)]
     public float rotationSpeed = 150f;
     private float angleStep;
+    [Range(0.01f, 5f)]
+    public float detectionRange = 2f;
+    [Range(0.01f, 5f)]
+    public float destroyRange = 0.2f;
+    [Range(0.1f, 5f)]
+    public float explosionDestroyTime = 1.5f;
+
+    [Header("Ability Toggles")]
+    public bool randomSpawnAngle = false;
+    public bool showCrosshair = true;
+    public bool destroyProjectileOnCollision = true;
+    public bool destroyNPCOnCollision = true;
+    public lockOn lockOnSetting;
+    public enum lockOn
+    {
+        DisableLockOn, RayCastLockOn, OverlapCircleLockOn, 
+    }
 
     public List<GameObject> projectiles;
+    public List<Vector3> targetPositions;
 
     [Header("Audio Sources")]
     public AudioSource readySound;
@@ -79,19 +97,17 @@ public class Ability_HellFire : Ability_Simple
         {
             for (int i = 0; i < projectiles.Count; i++)
             {               
-                if (projectiles[i] != null)
+                if (projectiles[i] != null && targetPositions[i] != null)
                 {
-                    Vector3 rotateTowards = (targetPosition - projectiles[i].transform.position).normalized;
+                    Vector3 rotateTowards = (targetPositions[i] - projectiles[i].transform.position).normalized;
                     float smoothRotateAngle = Vector2.SignedAngle(projectiles[i].transform.up, rotateTowards);
 
                     if (smoothRotateAngle > 0)
                     {
-                        // Rotate Left
                         projectiles[i].transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
                     }
                     else if (smoothRotateAngle < 0)
                     {
-                        // Rotate Right
                         projectiles[i].transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
                     }
 
@@ -104,22 +120,12 @@ public class Ability_HellFire : Ability_Simple
         {
             if (projectiles[i] != null)
             {
-                Physics2D.Raycast(projectiles[i].transform.position, projectiles[i].transform.up, 3f);
-                Debug.DrawRay(projectiles[i].transform.position, projectiles[i].transform.up * 3f, Color.red);
-
-
-                float distanceFromTarget = Vector2.Distance(targetPosition, projectiles[i].transform.position);
-
-                if (distanceFromTarget < 0.3f)
+                TargetRangeDetection(i);
+                if (lockOnSetting != lockOn.DisableLockOn)
                 {
-                    Destroy(projectiles[i]);
-
-                    explosionSound.Play();
-
-                    GameObject explosion;
-                    explosion = Instantiate(explosionPrefab, projectiles[i].gameObject.transform.position, projectiles[i].gameObject.transform.rotation);
-                    Destroy(explosion, 1.5f);
+                    NPCLockOnDetection(i);
                 }
+                NPCHitDetection(i);
             }
         }
 
@@ -130,7 +136,7 @@ public class Ability_HellFire : Ability_Simple
     {
         playerSpriteRenderer.color = Color.white;
         readySound.Play();
-
+        
         base.StartReady();
     }
 
@@ -139,28 +145,51 @@ public class Ability_HellFire : Ability_Simple
         playerSpriteRenderer.color = Color.green;
         windupSound.Play();
 
-        targetPosition = transform.position + Vector3.up * targetDistance;
+        crosshairPosition = transform.position + Vector3.up * targetDistance;
 
-        GameObject crosshair;       
-        crosshair = Instantiate(crosshairPrefab, targetPosition, Quaternion.identity);
-        Destroy(crosshair, activatedAbility_WindupTimer + activatedAbility_FiringTimer + activatedAbility_WinddownTimer);
+        for (int i = 0; i < projectileCount; i++)
+        {
+            Vector3 target = transform.position + Vector3.up * targetDistance;
+            targetPositions.Add(target);
+        }
 
+        if (showCrosshair)
+        {
+            if (crosshairPrefab != null)
+            {
+                GameObject crosshair;
+                crosshair = Instantiate(crosshairPrefab, crosshairPosition, Quaternion.identity);
+                Destroy(crosshair, activatedAbility_WindupTimer + activatedAbility_FiringTimer + activatedAbility_WinddownTimer);
+            }
+            else
+            {
+                Debug.LogError(gameObject.name + " is missing a reference to a crosshair prefab.");
+            }
+        }
 
         base.StartWindup();
     }
 
     protected override void StartFiring()
     {
-        playerSpriteRenderer.color = Color.red;       
+        playerSpriteRenderer.color = Color.grey;
+
+        angleStep = 360f / projectileCount;
 
         for (int i = 0; i < projectileCount; i++)
         {
-            angleStep = 360f / projectileCount;
             float angle = i * angleStep;
-            Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+            Quaternion rotation;
+            if (randomSpawnAngle) 
+            {
+                rotation = Quaternion.Euler(0f, 0f, Random.Range(0, 360));
+            }
+            else 
+            {
+                rotation = Quaternion.Euler(0f, 0f, angle);
+            }
 
             GameObject projectile;
-            //projectile = Instantiate(projectilePrefab, transform.position, Random.rotation, gameObject.transform);
             projectile = Instantiate(projectilePrefab, transform.position, rotation, gameObject.transform);
             projectile.name = "Projectile " + i;
             projectiles.Add(projectile);
@@ -168,20 +197,12 @@ public class Ability_HellFire : Ability_Simple
             launchSound.Play();
         }
 
-        for (int i = 0; i < projectiles.Count; i++)
-        {
-            if (projectiles[i] != null)
-            {
-
-            }
-        }
-
         base.StartFiring();
     }
 
     protected override void StartWinddown()
     {
-        playerSpriteRenderer.color = Color.grey;
+        playerSpriteRenderer.color = Color.red;        
         winddownSound.Play();
 
         for (int i = 0; i < projectiles.Count; i++)
@@ -189,6 +210,14 @@ public class Ability_HellFire : Ability_Simple
             if (projectiles[i] != null)
             {
                 firingSound.Play();
+
+                ParticleSystem particles;
+                particles = projectiles[i].GetComponentInChildren<ParticleSystem>();
+
+                if (particles != null)
+                {
+                    particles.Play();
+                }
             }
         }
 
@@ -210,12 +239,125 @@ public class Ability_HellFire : Ability_Simple
 
                 GameObject explosion;
                 explosion = Instantiate(explosionPrefab, projectiles[i].gameObject.transform.position, projectiles[i].gameObject.transform.rotation);
-                Destroy(explosion, 1.5f);
+                Destroy(explosion, explosionDestroyTime);
             }
         }
 
         projectiles.Clear();
+        targetPositions.Clear();
 
         base.StartCooldown();
+    }
+
+    private void TargetRangeDetection(int i)
+    {
+        float distanceFromTarget = Vector2.Distance(crosshairPosition, projectiles[i].transform.position);
+
+        if (distanceFromTarget < 0.3f)
+        {
+            Destroy(projectiles[i]);
+
+            explosionSound.Play();
+
+            GameObject explosion;
+            explosion = Instantiate(explosionPrefab, projectiles[i].transform.position, projectiles[i].transform.rotation);
+            Destroy(explosion, explosionDestroyTime);
+        }
+    }
+
+    private void NPCLockOnDetection(int i)
+    {
+        SpriteRenderer projectileSprite = projectiles[i].GetComponentInChildren<SpriteRenderer>();
+        if (projectileSprite == null)
+        {
+            Debug.Log(gameObject.name + "'s Projectile Prefab is missing a SpriteRenderer");
+            return;
+        }
+
+        if (lockOnSetting == lockOn.RayCastLockOn)
+        {
+            RaycastHit2D detectionRay;
+            detectionRay = Physics2D.Raycast(projectiles[i].transform.position, projectiles[i].transform.up, detectionRange, NPCLayers);
+
+            if (detectionRay.collider != null)
+            {
+                targetPositions[i] = detectionRay.collider.transform.position;
+                projectileSprite.color = Color.red;
+            }
+            else
+            {
+                targetPositions[i] = crosshairPosition;
+                projectileSprite.color = Color.white;
+            }
+        }
+        else if (lockOnSetting == lockOn.OverlapCircleLockOn)
+        {
+            Collider2D[] detectionCircle;
+            detectionCircle = Physics2D.OverlapCircleAll(projectiles[i].transform.position, detectionRange, NPCLayers);
+
+            for (int j = 0; j < detectionCircle.Length; j++)
+            {
+                if (detectionCircle[j] != null)
+                {
+                    targetPositions[i] = detectionCircle[j].transform.position;
+                    projectileSprite.color = Color.red;
+                }
+                else
+                {
+                    targetPositions[i] = crosshairPosition;
+                    projectileSprite.color = Color.white;
+                }
+            }
+        }
+    }
+
+    private void NPCHitDetection(int i)
+    {
+        RaycastHit2D hitRay;
+        hitRay = Physics2D.Raycast(projectiles[i].transform.position, projectiles[i].transform.up, destroyRange, NPCLayers);
+
+        if (hitRay.collider != null)
+        {
+            if (destroyProjectileOnCollision)
+            {
+                Destroy(projectiles[i]);
+                explosionSound.Play();
+
+                GameObject explosion;
+                explosion = Instantiate(explosionPrefab, projectiles[i].transform.position, projectiles[i].transform.rotation);
+                Destroy(explosion, explosionDestroyTime);
+            }             
+
+            if (destroyNPCOnCollision)
+            {
+                Destroy(hitRay.collider.gameObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (DEBUG_MODE)
+        {
+            for (int i = 0; i < projectiles.Count; i++)
+            {
+                if (projectiles[i] != null)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawRay(projectiles[i].transform.position, projectiles[i].transform.up * destroyRange);
+                    
+                    if (lockOnSetting == lockOn.OverlapCircleLockOn)
+                    {
+                        Gizmos.color = Color.blue;
+                        Gizmos.DrawWireSphere(projectiles[i].transform.position, detectionRange);
+                    }
+                    else if (lockOnSetting == lockOn.RayCastLockOn)
+                    {
+                        Gizmos.color = Color.yellow;
+                        Gizmos.DrawRay(projectiles[i].transform.position, projectiles[i].transform.up * destroyRange);
+                    }
+                }
+            }
+        }
     }
 }
