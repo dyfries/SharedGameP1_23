930c007a96d3b54c7186fc83f8aab5c3afb1bf88
agamis28 <agamis@rrc.ca>
2023-11-23 04:13:43 -0600

    Added health system for invincibility player, as well as a collision detection system. Changed speed on faster variation of minons

---
+++ Assets/Devs/Dev_Angel/Scripts/InvinsibilityPlayerHealth.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincibilityPlayerHealth : MonoBehaviour
+{
+    [Header("Health")]
+    private int currentHealth = 100;
+    public int CurrentHealth => currentHealth;
+
+    [Header("Animations")]
+    [SerializeField] private Animator anim;
+    [SerializeField] private PlayerArtController playerArtController;
+
+    [Header("** Debug Console Logs **")]
+    [SerializeField] private bool debug_Health;
+	[SerializeField] private bool debug_Damaged;
+
+    public void Damage(int damage)
+    {
+		// Lower health
+        currentHealth -= damage;
+
+		// If if player is dead
+		CheckIfDied();
+
+		// Debugs if debug_Damaged is true
+		if (debug_Damaged) { Debug.Log(gameObject.name + " was damaged!");  }
+    }
+
+	// Check if players health is too low and run animation and destroy object
+	private void CheckIfDied()
+	{
+		// Check health only when animation references aren't null
+		if (anim != null && playerArtController != null)
+		{
+			// If health goes under 0, start animation and destroy self after animation time
+			// And turn off art controller script to be able to use animator
+			if (currentHealth <= 0)
+			{
+				playerArtController.enabled = false;
+				anim.SetTrigger("Explode");
+				Destroy(gameObject, 1.083f);
+			}
+		}
+	}
+	private void Update()
+	{
+		// Debugs if debug_Health is true
+		if (debug_Health) { Debug.Log(gameObject.name + " Current Health: " + currentHealth); }
+	}
+}
