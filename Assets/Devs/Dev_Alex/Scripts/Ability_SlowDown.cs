715a47a28073dc56ae3fe400b08f5e569e84339d
Alex <abacon2@academic.rrc.ca>
2023-11-29 23:11:23 -0600

    Added sound effects

---
+++ Assets/Devs/Dev_Alex/Scripts/Ability_SlowDown.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_SlowDown : Ability_Simple
+{
+    [Header("Slow Down Ability Settings")]
+    [SerializeField] private int radius = 5; //radius of player 
+    [SerializeField] private float slowDownRate = 0.5f; // rate to add to drag to slow down object
+
+    private List<GameObject> objectsInRadius = new List<GameObject>();  // a list of the objects within radius
+    private float initialDrag;  // A variable to kepe track of the objects initial drag
+    public GameObject freezeBlock; //freeze block object
+    private List<GameObject> frozenBlocks = new List<GameObject>();  //list of frozen blocks
+    public List<Animator> blockAnimators = new List<Animator>(); //list of animators on blocks
+
+    private SoundManager soundManager;
+    protected void Start()
+    {
+        gameObject.AddComponent<CircleCollider2D>(); //add a circle collider
+        gameObject.GetComponent<CircleCollider2D>().isTrigger = true; //makes the collider a trigger
+        gameObject.GetComponent<CircleCollider2D>().radius = radius; //sets player's radius to the radius variable
+
+        //assign sounds
+        soundManager = gameObject.GetComponentInChildren<SoundManager>();
+    }
+
+    // Update is called once per frame
+    protected override void StartWindup()
+    {
+        base.StartWindup();
+
+    }
+    protected override void StartFiring()
+    {
+
+        soundManager.PlayFreezeSound();
+        base.StartFiring();
+        SlowDown(); //call slow down
+
+        //animation
+        //going to implement a freeze blast animation and an animation that freezes objects in radius
+        for (int i = 0; i < objectsInRadius.Count; i++)
+        {
+            if (objectsInRadius[i].GetComponent<SpriteRenderer>() != null && objectsInRadius[i].name.Contains("NPC")) //had to check contains npc so walls dont get blocks
+            {
+                GameObject block = Instantiate(freezeBlock, objectsInRadius[i].transform.position, objectsInRadius[i].transform.rotation, objectsInRadius[i].transform); //puts the object in a freeze block
+                frozenBlocks.Add(block);
+                blockAnimators.Add(block.GetComponent<Animator>());
+
+            }
+        }
+        StartMelting();
+    }
+
+    protected override void StartWinddown()
+    {
+        base.StartWinddown();
+        StopSlowDown(); //reverse slow down
+
+        for (int i = 0; i < frozenBlocks.Count; i++)
+        {
+            blockAnimators[i].ResetTrigger("start melting");
+            Destroy(frozenBlocks[i].gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            objectsInRadius.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (objectsInRadius.Contains(collision.gameObject))
+        {
+            objectsInRadius.Remove(collision.gameObject);
+        }
+    }
+
+    private void SlowDown()
+    {
+        for (int i = 0; i < objectsInRadius.Count; i++)
+        {
+            initialDrag = objectsInRadius[i].gameObject.GetComponent<Rigidbody2D>().drag;
+            objectsInRadius[i].gameObject.GetComponent<Rigidbody2D>().drag = initialDrag + slowDownRate;
+        }
+    }
+
+    private void StopSlowDown()
+    {
+        for (int i = 0; i < objectsInRadius.Count; i++) // go through every object in radius
+        {
+            objectsInRadius[i].gameObject.GetComponent<Rigidbody2D>().drag = -slowDownRate; //reverse slow down rate
+            if (objectsInRadius[i].gameObject.name.Contains("NPC")) //check if game object is an NPC
+            {
+                Rigidbody2D rb = objectsInRadius[i].gameObject.GetComponent<Rigidbody2D>();
+                float forceAmount = objectsInRadius[i].gameObject.GetComponent<BaseNPC>().startMoveForceY;
+                rb.AddForce(new Vector2(0, forceAmount), ForceMode2D.Impulse); //so speed continues, using impulse right now
+            }
+        }
+        soundManager.waterDripSound.Stop();
+    }
+
+    private void StartMelting()
+    {
+        soundManager.PlayWaterDripSound();
+        for (int i = 0; i < blockAnimators.Count; i++)
+        {
+            blockAnimators[i].SetTrigger("start melting");
+        }
+    }
+
+
+}
