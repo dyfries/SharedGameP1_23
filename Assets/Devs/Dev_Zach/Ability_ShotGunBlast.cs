d20e2cc1329155b8669359dffaf7d5b7f0933260
Zachery <zgilbert@rrc.ca>
2023-12-01 00:49:58 -0600

    more script chnages and scene changes

--- Assets/Devs/Dev_Zach/Ability_ShotGunBlast.cs
+++ Assets/Devs/Dev_Zach/Ability_ShotGunBlast.cs
@@ -6,8 +6,8 @@ public class Ability_ShotGunBlast : Ability_Simple
 {
 
     [Header("Now in Ability ShotGunBlast Subclass")]
-    public Rigidbody2D projectile;                                                                                                                                                                            //rigidbody2d?
-    public Transform startPoint;                                                                                                                                                                                    //Vector2
+    public Rigidbody2D projectile;     //rigidbody2d?
+    public Transform startPoint;       //Vector2
 
     public Rigidbody2D[] allBullets;// only one bullet obj to be able to use
 
@@ -19,6 +19,10 @@ public class Ability_ShotGunBlast : Ability_Simple
     private int bulletAmount = 3;
     public float bulletForce = 10f;
 
+    [SerializeField]
+    private SpriteRenderer windupRenderer;
+    public Sprite windup1;
+
     protected void Start()
     {
         if (projectile == null)
@@ -27,7 +31,16 @@ public class Ability_ShotGunBlast : Ability_Simple
             enabled = false;
         }
 
-                                                                                                                                                                                                        //allBullets = new Rigidbody2D[bulletAmount];
+        windupRenderer = GetComponent<SpriteRenderer>();
+
+                               //allBullets = new Rigidbody2D[bulletAmount];
+    }
+
+    protected override void StartWindup()
+    {
+        base.StartWindup();
+
+
     }
 
     protected override void StartFiring()
@@ -36,28 +49,24 @@ public class Ability_ShotGunBlast : Ability_Simple
         base.StartFiring();
 
         // Add projectiles
-                                                                                                                                                                                                            /*GameObject newProjectile = */
-        Rigidbody2D firstBullet = Instantiate(projectile, startPoint);                                                                                                                              /*, Quaternion.identity*/
+              
+        Rigidbody2D firstBullet = Instantiate(projectile, startPoint);          
         firstBullet.SetRotation( Quaternion.identity );
         firstBullet.AddForce(Vector2.up * bulletForce, ForceMode2D.Impulse);
         for (int i = 1; i < bulletAmount/2+.5; i++)
         {
-            Rigidbody2D newBulletRight = Instantiate(projectile, startPoint);                                                                                                                   /*, Quaternion.Euler(0, 0, -turnAngle * i)*/
+            Rigidbody2D newBulletRight = Instantiate(projectile, startPoint);     
 
             newBulletRight.SetRotation(Quaternion.Euler(0, 0, -turnAngle * i));
             newBulletRight.AddRelativeForce(Vector2.up * bulletForce, ForceMode2D.Impulse);
 
-            Rigidbody2D newBulletLeft = Instantiate(projectile, startPoint);                                                                                                                        /*, Quaternion.Euler(0,0,turnAngle * i)*/
+            Rigidbody2D newBulletLeft = Instantiate(projectile, startPoint);  
             newBulletLeft.SetRotation(Quaternion.Euler(0, 0, turnAngle * i));
             newBulletLeft.AddRelativeForce(Vector2.up * bulletForce, ForceMode2D.Impulse);
 
             
             
         }
-        
-                                                                                                                                                                                                    /*Instantiate(projectile, startPoint);
-                                                                                                                                                                                                    Instantiate(projectile, startPoint);*/
-
 
     }
 
