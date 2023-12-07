04333b134f931ad1d9137e379b2ba5605f2e6356
Dylan Fries <dyfries@rrc.ca>
2023-12-07 12:10:29 -0600

    Updates to the Koch Curve to make it a little more interesting.

--- Assets/LessonBundles/Recursion/KochCurve.cs
+++ Assets/LessonBundles/Recursion/KochCurve.cs
@@ -1,39 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class KochCurve : MonoBehaviour
 {
-    public int order = 4; // (depth of recursion). 
-    public float length = 1f;
+    public int maxOrder = 4; // (depth of recursion). 
+    public float startLength = 1f;
     public float rotationAngle = 60f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(0,1f)]
+    public float upDirectionRandomProb = 1f; // add random variance to the direction of the points. .5 
+
+    public float updateTimer = 2f; // regenerate each this many seconds. 
+    private float currentUpdateTimer = 0;
+
+    private bool ignoreSteps = false;
 
     void DrawCurve(int currentOrder, Vector3 start, Vector3 end, Color curveColor) {
-        Vector3 segment = end - start;
-        segment = segment / 3f;
-        Vector3 midPoint1 = start + segment;
-        Vector3 midPoint2 = start + segment*2;
 
-        Vector3 rotatedOffset = Quaternion.AngleAxis(rotationAngle, Vector3.forward) * segment;
+        if( currentOrder <= maxOrder) {
+            Vector3 segment = end - start;
+            segment = segment / 3f;
+            Vector3 midPoint1 = start + segment;
+            Vector3 midPoint2 = start + segment * 2;
 
-        Vector3 pointOfCurve = midPoint1 + rotatedOffset;
+            // This value will get replaced but needs to live outside of the if statement. 
+            Vector3 rotatedOffset = Vector3.up;
 
-        Debug.DrawLine(start, midPoint1, curveColor);
-        Debug.DrawLine(midPoint1, pointOfCurve, curveColor);
-        Debug.DrawLine(pointOfCurve, midPoint2, curveColor);
-        Debug.DrawLine(midPoint2, end, curveColor);
+            // If this is checked, randomly flip the direction of the points from the line. 
 
+            if(Random.value > upDirectionRandomProb) {
+                rotatedOffset = Quaternion.AngleAxis(rotationAngle, Vector3.forward) * segment;
+            } else {
+                rotatedOffset = Quaternion.AngleAxis(-rotationAngle, Vector3.forward) * segment;
+            }   
+
+            Vector3 pointOfCurve = midPoint1 + rotatedOffset;
+
+            if(currentOrder == maxOrder) {
+                curveColor = Color.red;
+            }
+
+            // Draw the actual curve
+            // update timer was added as duration since we are now only generating this once every t seconds. 
+            Debug.DrawLine(start, midPoint1, curveColor, updateTimer);
+            Debug.DrawLine(midPoint1, pointOfCurve, curveColor, updateTimer);
+            Debug.DrawLine(pointOfCurve, midPoint2, curveColor, updateTimer);
+            Debug.DrawLine(midPoint2, end, curveColor, updateTimer);
+
+            // Recursive Case
+            DrawCurve(currentOrder + 1, start, midPoint1, curveColor);
+            DrawCurve(currentOrder + 1, midPoint1, pointOfCurve, curveColor);
+            DrawCurve(currentOrder + 1, pointOfCurve, midPoint2, curveColor);
+            DrawCurve(currentOrder + 1, midPoint2, end, curveColor);
+        }
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        DrawCurve(0, transform.position, transform.position + Vector3.right * startLength, Color.white);
+        currentUpdateTimer = updateTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DrawCurve(order, transform.position, transform.position + Vector3.right * length, Color.white);
+        currentUpdateTimer -= Time.deltaTime;
+        if(currentUpdateTimer < 0) {
+            DrawCurve(0, transform.position, transform.position + Vector3.right * startLength, Color.white);
+            currentUpdateTimer = updateTimer;
+        }
+
     }
 }
